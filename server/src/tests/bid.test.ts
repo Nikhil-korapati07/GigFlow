13edcdb743919def9147ddc06d38d1dfb03ea2d0
Nikhil <nikhil@gmail.com>
2026-01-15 00:05:01 +0530

    Fix CORS for frontend

--- server/src/tests/bid.test.ts
+++ server/src/tests/bid.test.ts
@@ -12,7 +12,7 @@ import { Gig } from '../models/Gig';
 import { Bid } from '../models/Bid';
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
